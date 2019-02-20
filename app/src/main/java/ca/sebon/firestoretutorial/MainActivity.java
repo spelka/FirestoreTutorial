b68600189ffb67edca15557e41ab55a90cc763af
spelka <sebastian.pelka@hotmail.com>
2019-02-20 00:25:07 -0800

    Added a snapshot listener to fetch data in realtime as soon as there are ch anges in the DB document

--- app/src/main/java/ca/sebon/firestoretutorial/MainActivity.java
+++ app/src/main/java/ca/sebon/firestoretutorial/MainActivity.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editTextTitle;
     private EditText editTextDescription;
+    private TextView textViewData;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference noteRef = db.collection("Notebook").document("My First Note Document");
+    private ListenerRegistration noteListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,40 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextDescription = findViewById(R.id.edit_text_description);
+        textViewData = findViewById(R.id.text_view_data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Attach event listener in onStart
+        noteListener = noteRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                //check if something went wrong, and stop if there is a problem
+                if (e != null)
+                {
+                    Toast.makeText(MainActivity.this, "Error while loading!", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
+                    return;
+                }
+
+                if (documentSnapshot.exists())
+                {
+                    String title = documentSnapshot.getString(KEY_TITLE);
+                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                    textViewData.setText("Title: " + title + "\n" + "Description: " + description);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Detatach event listener in onStop, to prevent bandwidth usage in the background
+        noteListener.remove();
     }
 
     public void saveNote(View v)
@@ -47,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         note.put(KEY_DESCRIPTION, description);
 
         // add the note to the DB
-        db.collection("Notebook").document("My First Note Document").set(note)
+        noteRef.set(note)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -62,4 +107,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public void loadNote(View v)
+    {
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        //confirm the document snapshot exists before doing work
+                        if (documentSnapshot.exists())
+                        {
+                            String title = documentSnapshot.getString(KEY_TITLE);
+                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                            //For Reference, an alternate way to get data back into an object
+                            //Map<String,Object> note = documentSnapshot.getData();
+
+                            textViewData.setText("Title: " + title + "\n" + "Description: " + description);
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Error: Document not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+
+
 }
